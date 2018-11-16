8ce363db77f5c1d058490a0409fd76a3caca6445
Obsiye <mrabdirizak@gmail.com>
2018-11-16 16:19:43 +0000

    Update ComputerPlayer class and test to work

--- lib/computerplayer.rb
+++ lib/computerplayer.rb
@@ -1,2 +1,11 @@
 class ComputerPlayer
+
+  def self.moves
+    ["rock", "paper", "scissors"]
+  end
+
+  def self.random_move
+    self.moves[rand(0...self.moves.length)]
+  end
+
 end
--- spec/computerplayer_spec.rb
+++ spec/computerplayer_spec.rb
@@ -5,8 +5,8 @@ describe ComputerPlayer do
   let(:computer) { ComputerPlayer }
 
   it 'should return a random move that is either rock or paper or scissors' do
-    move = computer.move
-    expect(move).to be_in(["rock", "paper", "scissors"])
+    move = computer.random_move
+    expect(["rock", "paper", "scissors"]).to include(move)
   end
 
 end
