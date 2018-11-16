0286ef02dea200e11fe6413ecf932c21905f4873
Obsiye <mrabdirizak@gmail.com>
2018-11-16 10:20:00 +0000

    Feature test for filling in index user form

--- spec/features/index_spec.rb
+++ spec/features/index_spec.rb
@@ -4,5 +4,11 @@ feature "index page" do
     visit('/')
     expect(page).to have_content("Rock Paper Scissors!!")
   end
+
+  scenario 'Fill in user form' do
+    visit('/')
+    fill_in('name', with: 'Abdi')
+    expect(page).to have_content("Welcome Abdi")
+  end
   
 end
