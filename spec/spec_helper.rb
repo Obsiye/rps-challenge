a41dd881324bc87dde9f4a3646825ff5b63a4d98
Obsiye <mrabdirizak@gmail.com>
2018-11-16 14:49:40 +0000

    Rename extension on play_spec.erb to .rb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -19,9 +19,9 @@ RSpec.configure do |config|
   require_relative 'features/web_helpers.rb'
   Capybara.app = RockPaperScissors
 
-  config.after(:suite) do
-    puts
-    puts "\e[33mHave you considered running rubocop? It will help you improve your code!\e[0m"
-    puts "\e[33mTry it now! Just run: rubocop\e[0m"
-  end
+  # config.after(:suite) do
+  #   puts
+  #   puts "\e[33mHave you considered running rubocop? It will help you improve your code!\e[0m"
+  #   puts "\e[33mTry it now! Just run: rubocop\e[0m"
+  # end
 end
